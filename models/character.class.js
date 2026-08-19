21f78e1a86eb362dde6e81e0255de0157ae06c2c
chrisk2708 <chris.k2708@gmail.com>
2026-08-19 03:50:08 +0200

    create objects

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MoveableObject {
-
+    y = 140;
+    height = 300;
+    
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,13 @@
 class Chicken extends MoveableObject {
 
+    y = 370;
+    height = 50;
+    width = 50;
+
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+        this.x = 200 + Math.random() * 500;
     }
 
 }
